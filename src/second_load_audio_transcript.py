c68c4897f6b0a7ead21df7675ac798f5bed79d81
grpasfica <igraciasriska1262@gmail.com>
2025-09-18 11:07:26 +0700

    add function for transcribe audio using whisper

--- src/second_load_audio_transcript.py
+++ src/second_load_audio_transcript.py
@@ -1,6 +1,14 @@
 import pandas as pd
+import whisper
 
-# Load the CSV file into a DataFrame
+# function to transcript audio using whisper
+# for now not used whisper because it takes too long and not accurate for my audio
+def transcribe_audio(audio_path):
+    model = whisper.load_model("small")
+    result = model.transcribe(audio_path)
+    return result["text"]
+
+# Load the CSV file into a DataFrame (this data be obtained from manual transcription)
 def load_audio_transcript(csv_path):
     df = pd.read_csv(csv_path)
     return df
